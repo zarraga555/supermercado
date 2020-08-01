38e7b4cf06a0284f6066c651199dbaad35a91d63
zarraga555 <zarraga555@gmail.com>
2020-08-01 00:53:26 -0400

    cambios en la ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/home', function () {
+//     return view('dashboard');
+// });
+Route::resource('/home', 'dashboardController')->names('home');
+Route::resource('categoria', 'categoriasController')->names('categorias'); //LISTO
+Route::get('listCategoria/{page?}', 'categoriasController@listCategoria')->name('listCategoria');//List
+Route::resource('producto', 'productoController')->names('producto'); //LISTO
+Route::get('listProducto/{page?}', 'productoController@listProducto')->name('listProducto');//List
+Route::resource('proveedor', 'proveedorController')->names('proveedor'); //LISTO
+Route::get('listProveedor/{page?}', 'proveedorController@listProveedor')->name('listProveedor');//List
+Route::resource('pago', 'pagoController')->names('metodoPago');
+Route::get('listPago/{page?}', 'pagoController@listPago')->name('listPago');//List
+Route::resource('cliente', 'clienteController')->names('cliente'); //LISTO
+Route::get('listCliente/{page?}', 'clienteController@listCliente')->name('listCliente');//List
+//Route::resource('pedido', 'pedidoController')->names('pedido'); //LISTO
+//route::get('listPedido/{page?}', 'pedidoController@listPedido')->name('listPedido');//List
+Route::resource('cargo', 'cargoController')->names('cargo'); //LISTO
+route::get('listCargo/{page?}', 'cargoController@listCargo')->name('listCargo');//List
+Route::resource('empleado', 'empleadoController')->names('empleado'); //LISTO
+route::get('listEmpleado/{page?}', 'empleadoController@listEmpleado')->name('listEmpleado');//List
+Route::resource('usuario', 'usuarioController')->names('usuario'); //LISTO
+route::get('listUsuario/{page?}', 'usuarioController@listUsuario')->name('listUsuario');//List
+
+if(count(User::all()) >= 1){
+  Auth::routes([
+    'register' => false
+  ]);
+}else{
+  Auth::routes();
+}
+
